639fe19f38ec1666aeefb11004d98539637d8b73
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-26 19:27:18 +0300

    added get_progression_sum

--- homework-20-10-2025/main.cpp
+++ homework-20-10-2025/main.cpp
@@ -1,2 +1,27 @@
+#include <iostream>
+
+using u_t = unsigned;
+u_t get_progression_sum(u_t b, u_t q, u_t n);
+u_t add(u_t a, u_t b);
+u_t isAddErr(u_t res, u_t a, u_t b);
+u_t sub(u_t a, u_t b);
+u_t isSubErr(u_t res, u_t a, u_t b);
+u_t mp(u_t a, u_t b);
+u_t max_u();
+u_t min_u();
+
 int main()
-{}
+{
+    std::cout << get_progression_sum(2,2,3) << "\n";
+}
+
+u_t get_progression_sum(u_t b, u_t q, u_t n)
+{
+    u_t sum = b;
+    while (n > 1) {
+        b *= q;
+        sum += b;
+        n--;
+    }
+    return sum;
+}
