e6741da66c416f107d2d36d667e60fc135285734
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-31 22:19:01 +0300

    added gcd() and lcm()

--- homework-25-10-2025/main.cpp
+++ homework-25-10-2025/main.cpp
@@ -1,2 +1,30 @@
+#include <iostream>
+
+using ll_t = long long;
+
+ll_t gcd(ll_t a, ll_t b);
+ll_t lcm(ll_t a, ll_t b);
+
 int main()
-{}
+{
+  std::cout << gcd(155, 15) << "\n" << lcm(155, 15) << "\n";
+}
+
+
+ll_t gcd(ll_t a, ll_t b)
+{
+  while (a != 0 && b != 0){
+	  if (a > b){
+      a = a % b;
+    } else {
+      b = b % a;
+    }
+  }
+  return a+b;
+}
+
+
+ll_t lcm(ll_t a, ll_t b)
+{           
+  return (a * b) / gcd(a,b);
+}
