86173551c2982b2847aaedc0495b233accb4571c
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-10-31 23:39:25 +0300

    added abs(), deleted sqr, added add, added mp, added try and catch (TASK COMPLETE)

--- homework-25-10-2025/main.cpp
+++ homework-25-10-2025/main.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
+#include <limits>
 
 using ll_t = long long;
 
+ll_t max_ll();
+ll_t min_ll();
 ll_t gcd(ll_t a, ll_t b);
 ll_t lcm(ll_t a, ll_t b);
-ll_t sqr(ll_t a);
+ll_t abs(ll_t a);
+ll_t add(ll_t a, ll_t b);
+ll_t mp(ll_t a, ll_t b);
+
 
 int main()
 {
   ll_t a, last_lcm, sum_sqr;
   bool first = true;
   while (std::cin >> a) {
-    if (!first) { 
-      last_lcm = lcm(last_lcm, a);
-    } else {
-      last_lcm = a;
-      first = false;
+    try {
+      if (!first) { 
+        last_lcm = lcm(last_lcm, a);
+      } else {
+        last_lcm = a;
+        first = false;
+      }
+      sum_sqr = add(sum_sqr, mp(a, a));
+    } catch (const std::overflow_error &e){
+      std::cerr << e.what() << "\n";
+      return 1;
     }
-    sum_sqr += sqr(a);
   }
 
   if (std::cin.eof()) {
@@ -37,15 +48,50 @@ ll_t gcd(ll_t a, ll_t b)
       b = b % a;
     }
   }
-  return a+b;
+  return add(a, b);
+}
+
+ll_t max_ll()
+{
+  return std::numeric_limits<ll_t>::max();
+}
+
+ll_t min_ll()
+{
+  return std::numeric_limits<ll_t>::min();
+}
+
+ll_t abs(ll_t a)
+{
+  if (min_ll() != a) {
+    if (a < 0) {
+      return -a;
+    }
+    return a;
+  }
+  throw 
+    std::overflow_error("Overflow in abs");
 }
 
 ll_t lcm(ll_t a, ll_t b)
 {           
-  return (a * b) / gcd(a,b);
+  return (mp(a, b)) / gcd(a,b);
 }
 
-ll_t sqr(ll_t a)
+ll_t add(ll_t a, ll_t b)
 {
-  return a*a;
+  if (a < max_ll() - b){
+    return a+b;
+  }
+  throw
+    std::overflow_error("Overflow in add");
+}
+
+ll_t mp(ll_t a, ll_t b)
+{
+  if (max_ll() / a > b){
+    return a*b;
+  }
+  throw
+    std::overflow_error("Overflow in multiply");
 }
